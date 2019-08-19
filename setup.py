07d37579a3a65770b839ffc87872bb8e90913bcb
Atulya Kumar Pandey <atulya.pandey@datagrokr.com>
2019-08-19 23:28:37 +0530

    updated gitignore and setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='oktactl',
-      version='0.0.1',
+      version='0.0.2',
       description='Sample Okta CLI tool in python',
       license='MIT',
       author='Atulya Kumar Pandey',
